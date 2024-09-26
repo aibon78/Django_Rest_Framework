60aa39ee44e4b819749b2177ef334536b6d025da
André Filipe <130567849+aibon78@users.noreply.github.com>
2024-09-26 15:24:36 -0300

    add class Estudante

--- escola/models.py
+++ escola/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+'''Dados necessários:
+
+Id
+Nome
+E-mail
+Não pode estar em branco
+CPF
+Máximo de 11 caracteres
+Data de Nascimento
+Número de Celular
+Máximo de 14 caracteres'''
+
+class Estudante(models.Model):
+    nome = models.CharField(max_length= 100)
+    email = models.EmailField(blank = False, max_length=100)
+    cpf = models.CharField(max_length= 11)
+    data_nascimento = models.DateField()
+    celular = models.CharField(max_length= 14)
+    
+    def __str__(self):
+        return self.nome
+    
